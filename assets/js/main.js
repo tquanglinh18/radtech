a514a458976c3a86367f3652c9b607179468d5b0
LinhTQ <linhtq@vtconline.vn>
2024-10-03 17:31:09 +0700

    introduction page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
 $(function () {
-
+  $("#lst-doctor").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+  });
 });
